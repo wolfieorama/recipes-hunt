7b5f75111ff9e0a0e9a50281ed4dbaaceb24f878
wolfieorama <wachirawamunyi@gmail.com>
2015-11-16 11:12:34 +0300

    added nested form renders

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   has_many :directions
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
-  validates :title, :descrition, :image, presence: true
+  validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
