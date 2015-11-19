9f7de54c4082d6c20b02d79ff375656df48690df
wolfieorama <wachirawamunyi@gmail.com>
2015-11-19 17:02:03 +0300

    playing with rules on ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,9 @@ class Ability
         can :manage, :all
       else
         can :read, :all
+        can :edit, Recipe
+        can :read, Direction
+        can :read, Ingredient
       end
     #
     # The first argument to `can` is the action you are giving the user
